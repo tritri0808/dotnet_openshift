589f14a6862a89a55b2fb1736fbc1f2ef7ec8f0d
ngo tri <kiemmatri@gmail.com>
2017-09-28 14:48:29 +0700

    Add github

---
+++ dotnet_openshift/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration; // 1st line added
+
+namespace dotnet_openshift
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            // 2nd line added
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            // 3rd line added
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseUrls(url) // 4th line added
+                .Build();
+
+            host.Run();
+        }
+    }
+}
